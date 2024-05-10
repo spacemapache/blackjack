66f2fb44e71f17a883d7d0977ef07b1658f591c9
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-05-09 17:21:40 -0700

    updated message display when the start game button is clicked

--- src/index.js
+++ src/index.js
@@ -5,33 +5,32 @@
 // function for display card 2 text (random number)
 // if else statements (if < 21, deal more, else if 21 congrats, else > 21 you're out)
 
-let card1 = Math.floor(Math.random() * 11 + 1);
+// add boolean variable if player is alive or has blackjack
+
+// 2 cards drawn for start game - must be random , show each number and sum
+
+let card1 = Math.floor(Math.random() * 13) + 1;
+let card2 = Math.floor(Math.random() * 13) + 1;
+
 let cardDisplay = document.getElementById("cards");
 let messageDisplay = document.getElementById("message");
 let sumDisplay = document.getElementById("sum");
 
-let card2 = Math.floor(Math.random() * 11 + 1);
+let isAlive = true;
+let hasBlackJack = false;
 
-// function to display card1 and if else statements
+// function to display cards and sum for starting game
 function startGame() {
-  cardDisplay.textContent = "Cards: " + card1;
+  cardDisplay.textContent = "Cards: " + card1 + "," + card2;
 
-  if (card1 <= 20) {
+  let sum = card1 + card2;
+  sumDisplay.textContent = "Sum: " + sum;
+
+  if (sum <= 20) {
     messageDisplay.textContent = "Would you like to draw another card?";
-  } else if (card1 === 21) {
+  } else if (sum === 21) {
     messageDisplay.textContent = "Congrats, you've won!!";
   } else {
     messageDisplay.textContent = "You lose!!";
   }
-}
-
-// draw another card
-function drawCard() {
-  cardDisplay.textContent = "Cards: " + card1 + ", " + card2;
-
-  // update sum to reflect card numbers
-  let sum = card1 + card2;
-  sumDisplay.textContent = "Sum: " + sum;
-
-  // create loop to draw another card if <= 20
 }
